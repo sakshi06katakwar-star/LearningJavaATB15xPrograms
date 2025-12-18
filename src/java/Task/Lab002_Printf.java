d4f723c3caac7c0f974eac9c86cba0561877de1c
Sakshi <sakshi06katakwar@gmail.com>
2025-12-18 12:08:16 +0530

    My 2nd Commit for Printf command

---
+++ src/java/Task/Lab002_Printf.java
@@ -0,0 +1,18 @@
+package Task;
+
+public class Lab002_Printf {
+    public static void main(String[] args) {
+        //Print the table of 9
+        int x = 9;
+        System.out.printf("%d*1=%d%n", x, x*1);
+        System.out.printf("%d*2=%d%n", x, x*2);
+        System.out.printf("%d*3=%d%n", x, x*3);
+        System.out.printf("%d*4=%d%n", x, x*4);
+        System.out.printf("%d*5=%d%n", x, x*5);
+        System.out.printf("%d*6=%d%n", x, x*6);
+        System.out.printf("%d*7=%d%n", x, x*7);
+        System.out.printf("%d*8=%d%n", x, x*8);
+        System.out.printf("%d*9=%d%n", x, x*9);
+        System.out.printf("%d*10=%d%n", x, x*10);
+    }
+}
